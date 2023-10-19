9ae683a18fcee124bfb970cacdfc8bae9b8c029c
paynesc1 <paynesc1@gmail.com>
2023-10-19 11:02:14 -0600

    Second Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("button").addEventListener("click", async function getCoderData(){
+        var response = await fetch("https://api.github.com/users/paynesc1")
+        var coderData = await response.json();
+        console.log(coderData);
+
+        var avatar = document.getElementById('avatar')
+        avatar.src = coderData.avatar_url;
+        avatar.style.display = "block";
+
+        var html_url = document.getElementById('html_url')
+        html_url.href = coderData.html_url;
+        html_url.style.display = "block";
+
+    })
+    getCoderData();
+});
