90a0a19aeaa35bfdd6e7b19a6ebc3ad5398e5265
paynesc1 <98866833+paynesc1@users.noreply.github.com>
2024-01-14 13:39:12 -0700

    Update server.py

--- server.py
+++ server.py
@@ -4,9 +4,9 @@ from flask import render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-  return("index.html")
+  return('index.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
